be4f86152b097d23233684ab04b1d5145b6c2175
voidsoul-host <sanskar.purohit946@gmail.com>
2024-02-28 22:45:43 +0530

    not complete but certificate cards done

--- src/Components/Certificate.js
+++ src/Components/Certificate.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import './certificate.css'
+import rkclCertificate from '../assets/1Certificate.jpeg'
 
 function Certificate() {
   return (
     <div className='certificate-container'>
         <h1>Certificate</h1>
         <p className='mb-0'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor vero, rem quia magnam necessitatibus quas eligendi dignissimos corporis ex illo reiciendis velit, distinctio ducimus temporibus aperiam iste iusto impedit minus quibusdam, perferendis cum. Cupiditate, quis.</p>
-      
+        <div className="cards mt-4">
+          <div className="card">
+            <div className="card-img">
+              <img className='certificate-img' src={rkclCertificate} alt="" />
+            </div>
+            <div className="card-text">
+              <span className='text'>MADE IN INDIA</span>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-img">
+              <img className='certificate-img' src={rkclCertificate} alt="" />
+            </div>
+            <div className="card-text">
+              <span className='text'>MADE IN INDIA</span>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-img">
+              <img className='certificate-img' src={rkclCertificate} alt="" />
+            </div>
+            <div className="card-text">
+              <span className='text'>MADE IN INDIA</span>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
