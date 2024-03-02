33dc7cfb5814b3bee41ba746933dda08ada7f264
voidsoul-host <sanskar.purohit946@gmail.com>
2024-03-02 18:34:54 +0530

    certificates section ended

--- src/Components/Certificate.js
+++ src/Components/Certificate.js
@@ -1,37 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './certificate.css'
-import rkclCertificate from '../assets/1Certificate.jpeg'
+import rkclCertificate from '../assets/certificates/1Certificate.jpeg'
+import dcoCertificate from '../assets/certificates/1Certificate.jpeg'
+import basicCertificate from '../assets/certificates/1Certificate.jpeg'
 
 function Certificate() {
+  const [certificates, setCertificates] = useState([
+    {
+      title: 'Typing Certificate',
+      imagePath: rkclCertificate,
+    },
+    {
+      title: 'DIPLOMA IN COMPUTER OPERATOR (DCO)',
+      imagePath: dcoCertificate,
+    },
+    {
+      title: 'Basic Fundamentals Certificate',
+      imagePath: basicCertificate,
+    },
+  ])
+  
   return (
     <div className='certificate-container'>
         <h1>Certificate</h1>
         <p className='mb-0'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor vero, rem quia magnam necessitatibus quas eligendi dignissimos corporis ex illo reiciendis velit, distinctio ducimus temporibus aperiam iste iusto impedit minus quibusdam, perferendis cum. Cupiditate, quis.</p>
         <div className="cards mt-4">
-          <div className="card">
-            <div className="card-img">
-              <img className='certificate-img' src={rkclCertificate} alt="" />
-            </div>
-            <div className="card-text">
-              <span className='text'>MADE IN INDIA</span>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-img">
-              <img className='certificate-img' src={rkclCertificate} alt="" />
-            </div>
-            <div className="card-text">
-              <span className='text'>MADE IN INDIA</span>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-img">
-              <img className='certificate-img' src={rkclCertificate} alt="" />
-            </div>
-            <div className="card-text">
-              <span className='text'>MADE IN INDIA</span>
-            </div>
-          </div>
+          {
+            certificates.map((certificate, index) =>{
+              console.log('hit')
+              return (
+                <div className="card" key={index}>
+                  <div className="card-img">
+                    <img className='certificate-img' src={certificate.imagePath} alt="" />
+                  </div>
+                  <div className="card-text">
+                    <span className='text'>{certificate.title}</span>
+                  </div>
+                </div>
+              )
+            })
+          }
         </div>
     </div>
   )
