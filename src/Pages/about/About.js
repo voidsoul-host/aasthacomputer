38dd4113a4f67caa7fbc9bede8fa48c9a697266f
voidsoul-host <sanskar.purohit946@gmail.com>
2024-03-03 23:55:58 +0530

    home and contact us completed without few finishings

--- src/Pages/about/About.js
+++ src/Pages/about/About.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import NavBar from '../../Components/NavBar'
-import AboutUs from '../../Components/Aboutus'
+import acelogo from '../../assets/acelogo.png'
+import './about.css'
 
 function About() {
   return (
     <div className='main-container'>
       <div className="container">
         <NavBar />
-        <AboutUs />
+        <div className="whoWeAre d-flex">
+          <div className="left leftWho">
+            <h1>WHO WE ARE</h1>
+            <p className='mb-0 mt-5'>We are a group of students who are passionate about technology and want to make the world a better place.</p>
+          </div>
+          <div className="right rightWho">
+            
+          <div className="about-box z-1 d-flex">
+            <div className="box-text">
+              <img src={acelogo} alt="whoWeAre" />
+            </div>
+          </div>
+
+          </div>
+        </div>
       </div>
     </div>
   )
